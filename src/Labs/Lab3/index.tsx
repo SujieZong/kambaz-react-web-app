3b97fad4047682129b8c093bbc4521209f7f8475
SujieZong <sjzong1016@outlook.com>
2025-07-17 14:55:07 -0700

    Add Lab 3 components for various JavaScript concepts including arrays, functions, and conditionals

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,36 @@
+import BooleanVariables from "./BooleanVariables";
+import TernaryOperator from "./TernaryOperator";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import IfElse from "./IfElse";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingDataToFromArrays";
 export default function Lab3() {
-    return (
-        <div>
-            <h2>Lab 3</h2>
-        </div>
-    );
+  return (
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions  />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+
+    </div>
+  );
 }
