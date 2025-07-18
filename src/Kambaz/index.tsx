174e3ace2368e03a363916abf36522af14d94b0f
SujieZong <sjzong1016@outlook.com>
2025-07-18 15:09:27 -0700

    Add Lab 3 components including Add, AddPathParameters, Classes, DestructingImports, Highlight, PathParameters, Square, and Styles for enhanced functionality and styling

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -3,13 +3,14 @@ import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KambazNavigation from "./Navigation";
 import Courses from "./Courses";
-import "./styles.css"; 
+import "./styles.css";
 export default function Kambaz() {
   return (
     <div id="wd-kambaz">
       <KambazNavigation />
-      <div className="wd-main-content-offset"> 
+      <div className="wd-main-content-offset p-3">
         <Routes>
+          
           <Route path="/" element={<Navigate to="Account" />} />
           <Route path="/Account/*" element={<Account />} />
           <Route path="/Dashboard" element={<Dashboard />} />
